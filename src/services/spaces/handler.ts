82cf2de2dccc0cb6122c9584c08f39b6ee7a6ea1
Amilcar Sanchez <amilcaralex97@gmail.com>
2023-11-24 10:22:47 -0600

    serverless post item

--- src/services/spaces/handler.ts
+++ src/services/spaces/handler.ts
@@ -1,32 +1,46 @@
 import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
-  Context,
-} from "aws-lambda";
+	APIGatewayProxyEvent,
+	APIGatewayProxyResult,
+	Context,
+} from 'aws-lambda';
+import { postSpaces } from './PostSpaces';
+
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+
+const ddbClient = new DynamoDBClient({});
 
 async function handler(
-  event: APIGatewayProxyEvent,
-  context: Context
+	event: APIGatewayProxyEvent,
+	context: Context
 ): Promise<APIGatewayProxyResult> {
-  let message: string;
+	let message: string;
 
-  switch (event.httpMethod) {
-    case "GET":
-      message = "Hello from GET!";
-      break;
-    case "POST":
-      message = "Hello from POST!";
-      break;
-    default:
-      break;
-  }
+	try {
+		switch (event.httpMethod) {
+			case 'GET':
+				message = 'Hello from GET!';
+				break;
+			case 'POST':
+				const response = postSpaces(event, ddbClient);
+				return response;
+				break;
+			default:
+				break;
+		}
+	} catch (error) {
+		console.log(error);
+		return {
+			statusCode: 500,
+			body: JSON.stringify(error.message),
+		};
+	}
 
-  const response: APIGatewayProxyResult = {
-    statusCode: 200,
-    body: JSON.stringify(message),
-  };
+	const response: APIGatewayProxyResult = {
+		statusCode: 200,
+		body: JSON.stringify(message),
+	};
 
-  return response;
+	return response;
 }
 
 export { handler };
