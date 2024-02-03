0818720117f0c8039fc3264bef6cd4230e639c6d
Amilcar Sanchez <amilcaralex97@gmail.com>
2024-02-02 18:30:56 -0600

    create cognito identity pool

--- src/infra/stacks/AuthStack.ts
+++ src/infra/stacks/AuthStack.ts
@@ -1,59 +1,77 @@
-import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import {
-  CfnUserPoolGroup,
-  UserPool,
-  UserPoolClient,
-} from "aws-cdk-lib/aws-cognito";
-import { CfnUserGroup } from "aws-cdk-lib/aws-elasticache";
-import { Construct } from "constructs";
+	CfnIdentityPool,
+	CfnUserPoolGroup,
+	UserPool,
+	UserPoolClient,
+} from 'aws-cdk-lib/aws-cognito';
+import { CfnUserGroup } from 'aws-cdk-lib/aws-elasticache';
+import { Construct } from 'constructs';
 
 export class AuthStack extends Stack {
-  public userPool: UserPool;
-  private userPoolClient: UserPoolClient;
+	public userPool: UserPool;
+	private userPoolClient: UserPoolClient;
+	private idenitityPool: CfnIdentityPool;
 
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
 
-    this.createUserPool();
-    this.createUserPoolClient();
-    this.createAdminsGroup();
-  }
+		this.createUserPool();
+		this.createUserPoolClient();
+		this.createAdminsGroup();
+		this.createIdentityPool();
+	}
 
-  private createUserPool() {
-    this.userPool = new UserPool(this, "SpaceUserPool", {
-      selfSignUpEnabled: true,
-      signInAliases: {
-        username: true,
-        email: true,
-      },
-    });
+	private createUserPool() {
+		this.userPool = new UserPool(this, 'SpaceUserPool', {
+			selfSignUpEnabled: true,
+			signInAliases: {
+				username: true,
+				email: true,
+			},
+		});
 
-    new CfnOutput(this, "SpaceUserPoolId", {
-      value: this.userPool.userPoolId,
-    });
-  }
-  private createUserPoolClient() {
-    this.userPoolClient = this.userPool.addClient("SpaceUserPoolClient", {
-      authFlows: {
-        adminUserPassword: true,
-        custom: true,
-        userPassword: true,
-        userSrp: true,
-      },
-    });
-    new CfnOutput(this, "SpaceUserPoolClientId", {
-      value: this.userPoolClient.userPoolClientId,
-    });
-  }
+		new CfnOutput(this, 'SpaceUserPoolId', {
+			value: this.userPool.userPoolId,
+		});
+	}
+	private createUserPoolClient() {
+		this.userPoolClient = this.userPool.addClient('SpaceUserPoolClient', {
+			authFlows: {
+				adminUserPassword: true,
+				custom: true,
+				userPassword: true,
+				userSrp: true,
+			},
+		});
+		new CfnOutput(this, 'SpaceUserPoolClientId', {
+			value: this.userPoolClient.userPoolClientId,
+		});
+	}
 
-  private createAdminsGroup() {
-    new CfnUserPoolGroup(this, "SpaceAdmins", {
-      userPoolId: this.userPool.userPoolId,
-      groupName: "admins",
-    });
-  }
+	private createAdminsGroup() {
+		new CfnUserPoolGroup(this, 'SpaceAdmins', {
+			userPoolId: this.userPool.userPoolId,
+			groupName: 'admins',
+		});
+	}
 
-  getUserPool(): UserPool {
-    return this.userPool;
-  }
+	getUserPool(): UserPool {
+		return this.userPool;
+	}
+
+	private createIdentityPool() {
+		this.idenitityPool = new CfnIdentityPool(this, 'SpaceIdentityPool', {
+			allowUnauthenticatedIdentities: true,
+			cognitoIdentityProviders: [
+				{
+					clientId: this.userPoolClient.userPoolClientId,
+					providerName: this.userPool.userPoolProviderName,
+				},
+			],
+		});
+		new CfnOutput(this, 'SpaceIdenitityPool', {
+			value: this.idenitityPool.ref,
+		});
+	}
 }
